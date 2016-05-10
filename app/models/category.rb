25d4873aacad36ccf8b3d35a0c047f6d0e648cf9
Simone Borges <simone.borges@gmail.com>
2016-05-10 12:54:10 -0300

    Dois cruds funcionando, categoria e questoes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
+	validates :name, :description, :presence => true
+	has_many :questions
 end
---
+++ app/views/questions/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @question, :id, :body, :category_id, :created_at, :updated_at
