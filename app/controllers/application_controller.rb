6840422e7da58aa63a00569afc0495fbfe9dde52
Simone Borges <simone.borges@gmail.com>
2016-05-18 09:56:35 -0300

    retirei facebookcanvas call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
 
-  layout "facebook_canvas"
-
-  after_filter :allow_iframe
-
-  def index
-  end
-
-  private
-  
-  def allow_iframe
-    response.headers["X-Frame-Options"] = "GOFORIT"
-  end
-
 end
