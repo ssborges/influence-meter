e2d52e31c3f7f83cce097e5ae723d17c33b1349a
Simone Borges <simone.borges@gmail.com>
2016-05-18 17:34:51 -0300

    tentando funcionar o x-options no rails 4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,4 @@
 class HomeController < ApplicationController
-  after_action :allow_iframe, only: :embed
-
-  def embed
-  end
-
-private
-
-  def allow_iframe
-    response.headers.except! 'X-Frame-Options'
-  end
 
   def index
   end
