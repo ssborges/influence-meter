460ba5b90dae926b1ba7d422c2f725c91badd878
Simone Borges <simone.borges@gmail.com>
2016-05-18 15:55:54 -0300

    x-frame

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+  after_action :allow_iframe, only: :embed
+
+  def embed
+  end
+
+private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
+  end
 
   def index
   end
--- app/controllers/management_controller.rb
+++
@@ -1,8 +0,0 @@
-class ManagementController < ApplicationController
-
-  before_action :set_survey, only: [:show]	
-
-  def index
-  end
-  
-end
