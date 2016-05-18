c080d8208a4d1c7baf0bb2cc11b807b87f98ff08
Simone Borges <simone.borges@gmail.com>
2016-05-18 10:58:25 -0300

    retirei facebookcanvas call e login facebook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,6 @@
 class HomeController < ApplicationController
 
-  layout "facebook_canvas"
-
-  after_filter :allow_iframe
-
   def index
-  end
-
-  def index
-  end
-
-  private
-
-  def allow_iframe
-    response.headers['X-Frame-Options'] = "ALLOW-FROM https://www.facebook.com"
   end
 
 end
