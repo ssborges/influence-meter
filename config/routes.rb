891bae422de42debb78b7d0fa5fc46d86498047c
Simone Borges <simone.borges@gmail.com>
2016-05-18 11:46:59 -0300

    question_path ???

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :management
   resources :survey
+  resourcer :home
   
 
   # The priority is based upon order of creation: first created -> highest priority.
