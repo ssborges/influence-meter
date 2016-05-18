2ec19c3083e97cfb05c8f23682d79325b61bcd06
Simone Borges <simone.borges@gmail.com>
2016-05-18 13:51:13 -0300

    question_path ???

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
 
   resources :categories
-  
+
   resources :questions do  
-    resources :survey
+    resources :survey, only: :show
   end
 
   resources :management
