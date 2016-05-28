6d75a097776ba3443475b2eccc4aeeec0673a66b
Simone Borges <simone.borges@gmail.com>
2016-05-28 17:45:25 -0300

    Deu certo paǵina de resposta do survey salvando no banco

--- db/migrate/20160526130848_remove_question_from_surveys.rb
+++
@@ -1,5 +0,0 @@
-class RemoveQuestionFromSurveys < ActiveRecord::Migration
-  def change
-    remove_column :surveys, :question, :integer
-  end
-end
--- db/migrate/20160526130924_remove_option_from_surveys.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOptionFromSurveys < ActiveRecord::Migration
-  def change
-    remove_column :surveys, :option, :integer
-  end
-end
