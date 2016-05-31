f79ea3561957ac6712768461228b9feb858feec7
Simone Borges <simone.borges@gmail.com>
2016-05-31 11:49:11 -0300

    login do facebook com feedback

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,11 @@ Rails.application.routes.draw do
 
   resources :surveys
 
-  resources :options
+  get "/auth/:provider/callback" => "sessions#create", as: :auth_callback
+
+  get "/auth/failure" => "sessions#failure", as: :auth_failure
+
+  get "/logout" => "sessions#destroy", as: :logout
 
 
 
